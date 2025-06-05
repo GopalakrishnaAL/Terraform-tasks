2b7e49e36f479cde8ce5161071a88925a91953f6
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-06-05 07:31:46 +0530

    terraform structure

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "state-file-Oregon"
-    key          = "Oregon_terraform_file/Repo_1_Oregon.tfstate"
-    region       = "eu-west-2"
+    bucket       = "state-file-ireland"
+    key          = "ireland_terraform_file/repo_1_oregon.tfstate"
+    region       = "eu-west-1"
     encrypt      = true
     # use_lockfile = true
   }
