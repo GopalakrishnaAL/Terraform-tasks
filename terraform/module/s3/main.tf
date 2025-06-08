68f007872442cab01826ab239e95f9a2b52ee76a
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-06-08 21:49:55 +0530

    pushing terraform files for execution

--- terraform/module/s3/main.tf
+++ terraform/module/s3/main.tf
@@ -1,17 +1,18 @@
 # S3 Index bucket.
 resource "aws_s3_bucket" "index_bucket" {
-  bucket = lower(format("%s-%s", var.name, var.vpc_id))
+  bucket = lower(format("%s", var.name))
 }
-
 
 # Block bucket public access.
 resource "aws_s3_bucket_public_access_block" "index_bucket_block_public" {
-  bucket = var.name
+  bucket = aws_s3_bucket.index_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
-  restrict_public_buckets = true
   ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  depends_on = [aws_s3_bucket.index_bucket]
 }
 
 resource "aws_s3_bucket_policy" "vpc_only" {
