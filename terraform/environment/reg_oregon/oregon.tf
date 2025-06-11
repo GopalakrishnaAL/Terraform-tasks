4e845ac63a4fa59943c13e948085b67e442aaf96
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-06-12 04:30:58 +0530

    pushing terraform files for execution

--- terraform/environment/reg_oregon/oregon.tf
+++ terraform/environment/reg_oregon/oregon.tf
@@ -1,7 +1,6 @@
-# module "s3" {
-#   source = "../../module/s3"
+module "s3" {
+  source = "../../module/s3"
 
-#   name    = var.name
-#   vpc_id  = var.vpc_id
-# }
-# Decomission
+  name    = var.name
+  vpc_id  = var.vpc_id
+}
