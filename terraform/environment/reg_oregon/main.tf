31eb489fed658d6cf2e93eb52404ef3e39bfe43d
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-06-23 20:05:59 +0530

    pushing terraform files for execution

--- terraform/environment/reg_oregon/main.tf
+++ terraform/environment/reg_oregon/main.tf
@@ -3,8 +3,8 @@ module "ebs" {
 
   #volume_size       = var.volume_size
   ebs_volumes        = var.reg_ebs_volumes
-  availability_zone  = module.ec2.availability_zone
-  instance_id        = module.ec2.instance_id
+  availability_zone  = module.ec2.EC2_availabilty_zone
+  instance_id        = module.ec2.Ec2_Instance_id
 }
 
 module "ec2" {
@@ -13,7 +13,7 @@ module "ec2" {
   instance_name     = var.instance_name
   instance_type     = var.instance_type
   ami_id            = var.ami_id
-  security_group_ids = module.sg.SG_id
+  security_group_ids = [module.sg.SG_id]
 }
 
 module "s3" {
@@ -29,4 +29,12 @@ module "sg" {
 
   ingress_rules = var.ingress_rules
   vpc_id = module.s3.vpc_id
+}
+
+output "EC2_availabilty_zone" {
+  value = module.ec2.EC2_availabilty_zone
+}
+
+output "Ec2_Instance_id" {
+  value = module.ec2.Ec2_Instance_id
 }
