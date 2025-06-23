a6c1c68a251c723f7b5f3eab18b7a18b19ebdf86
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-06-23 16:42:05 +0530

    pushing terraform files for execution

--- terraform/oregon_main.tf
+++ terraform/oregon_main.tf
@@ -1,48 +1,47 @@
-# Fetch default VPC in Oregon region
-data "aws_vpc" "default" {
-  default = true
-}
+module "oregon" {
+  source = "./Environment/Reg_oregon"
 
-# S3 Module
-module "oregon_s3" {
-  source    = "./environment/reg_oregon/s3"
   providers = { aws = aws.oregon }
 
-  name           = "task-s3bucket-oregon"
-  vpc_id         = data.aws_vpc.default.id
+  # S3 Bucket Configuration
+  name = "test_bucket" 
+  vpc_id = data.aws_vpc.default.id
   lifecycle_days = 15
-}
 
-# EC2 Module
-module "oregon_ec2" {
-  source    = "./environment/reg_oregon/ec2"
-  providers = { aws = aws.oregon }
-
-  name              = "test-instance"
-  ami               = "ami-0fe630eb857a6ec83" # Windows Server 2019 in us-west-2
-  instance_type     = "t3.medium"
-  availability_zone = "us-west-2a" # Use specific AZ or module output
-
-  ingress_rules = [
+  # EC2 Instance Configuration
+  ec2_name         = "test_instance"
+  ami_id           = "ami-0fe630eb857a6ec83"  # Windows Server 2019 in us-west-2
+  instance_type    = "t3.medium"
+  availability_zone = "us-west-2a"
+  
+  # Security Group Rules
+  ingress_rules = [ 
     {
-      from_port   = 3389
-      to_port     = 3389
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-      description = "RDP access"
+      from_port    = 3389,
+      to_port      = 3389,
+      protocol     = "tcp",
+      cidr_blocks  = ["0.0.0.0/0"],
+      description  = "RDP access"
     }
   ]
 
+  # EBS Volumes
   ebs_volumes = [
     {
-      device_name = "/dev/sdh"
-      volume_size = 30
+      device_name = "/dev/sdh",
+      volume_size = 30,
       volume_type = "gp3"
     },
     {
-      device_name = "/dev/sdi"
-      volume_size = 20
+      device_name = "/dev/sdi",
+      volume_size = 20,
       volume_type = "gp3"
     }
   ]
+}
+
+# Fetch default VPC in Oregon region
+data "aws_vpc" "default" {
+  provider = aws.oregon
+  default  = true
 }
