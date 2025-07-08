a0f3fb6784b5db4b2c7fd3be910d79cda43e77b5
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-07-08 17:44:57 +0530

    post - Task 2 Full Code execution

--- terraform/oregon_main.tf
+++ terraform/oregon_main.tf
@@ -1,59 +1,59 @@
-# module "reg_oregon" {
-#   source = "./environment/reg_oregon"
+module "reg_oregon" {
+  source = "./environment/reg_oregon"
 
-#   providers = { aws = aws.oregon }
+  providers = { aws = aws.oregon }
 
-#   # S3 Bucket Configuration
-#   name           = "gopal-1-bucket"
-#   vpc_id         = data.aws_vpc.default.id
-#   lifecycle_days = 15
+  # S3 Bucket Configuration
+  name           = "gopal-1-bucket"
+  vpc_id         = data.aws_vpc.default.id
+  lifecycle_days = 15
 
-#   # EC2 Instance Configuration
-#   instance_name     = "test_instance"
-#   ami_id            = "ami-0992e77a19022a98d"
-#   instance_type     = "t3.micro"
-#   availability_zone = module.reg_oregon.EC2_availabilty_zone
-#   instance_id       = module.reg_oregon.Ec2_Instance_id
-#   root_vol = "80"
+  # EC2 Instance Configuration
+  instance_name     = "test_instance"
+  ami_id            = "ami-0992e77a19022a98d"
+  instance_type     = "t3.micro"
+  availability_zone = module.reg_oregon.EC2_availabilty_zone
+  instance_id       = module.reg_oregon.Ec2_Instance_id
+  root_vol = "80"
 
-#   # Security Group Rules
-#   sg1_ingress_rules = [ 
+  # Security Group Rules
+  sg1_ingress_rules = [ 
+    {
+      from_port   = 95,
+      to_port     = 95,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "RDP access"
+    }    
+  ]
+  sg2_ingress_rules = [ 
+    {
+      from_port   = 12,
+      to_port     = 12,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "RDP access"
+    }    
+  ]
+
+  # EBS Volumes
+#   reg_ebs_volumes = [
 #     {
-#       from_port   = 95,
-#       to_port     = 95,
-#       protocol    = "tcp",
-#       cidr_blocks = ["0.0.0.0/0"],
-#       description = "RDP access"
-#     }    
-#   ]
-#   sg2_ingress_rules = [ 
+#       device_name = "/dev/sdh",
+#       volume_size = 30,
+#       volume_type = "gp3"
+#       encrypted = true
+#     },
 #     {
-#       from_port   = 12,
-#       to_port     = 12,
-#       protocol    = "tcp",
-#       cidr_blocks = ["0.0.0.0/0"],
-#       description = "RDP access"
-#     }    
+#       device_name = "/dev/sdi",
+#       volume_size = 20,
+#       volume_type = "gp3"
+#       encrypted = true
+#     }
 #   ]
+}
 
-#   # EBS Volumes
-# #   reg_ebs_volumes = [
-# #     {
-# #       device_name = "/dev/sdh",
-# #       volume_size = 30,
-# #       volume_type = "gp3"
-# #       encrypted = true
-# #     },
-# #     {
-# #       device_name = "/dev/sdi",
-# #       volume_size = 20,
-# #       volume_type = "gp3"
-# #       encrypted = true
-# #     }
-# #   ]
-# }
-
-# data "aws_vpc" "default" {
-#   provider = aws.oregon
-#   default  = true
-# }
+data "aws_vpc" "default" {
+  provider = aws.oregon
+  default  = true
+}
