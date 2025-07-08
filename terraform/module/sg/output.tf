67eb0f6a7c30efa63b9b7df5d014be30a6b36be3
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-07-08 15:47:06 +0530

    pushing terraform files for execution

--- terraform/module/sg/output.tf
+++ terraform/module/sg/output.tf
@@ -1,7 +1,7 @@
 output "SG_id1" {
-  value = aws_security_group.sg1.id
+  value = aws_security_group.dynamic_sg1.id
 }
 
 output "SG_id2" {
-  value = aws_security_group.sg2.id
+  value = aws_security_group.dynamic_sg2.id
 }
