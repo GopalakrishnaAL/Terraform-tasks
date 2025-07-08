b5d3fd934c97e22f3fc86cdaf7413162941b525a
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-07-08 16:51:33 +0530

    pushing terraform files for execution

--- terraform/environment/reg_oregon/variable.tf
+++ terraform/environment/reg_oregon/variable.tf
@@ -1,74 +1,74 @@
-#EC2 variable
+# #EC2 variable
 
-variable "instance_name" {
-  type = string
-}
-
-variable "ami_id" {
-  type = string
-}
-
-variable "instance_type" {
-  type = string
-}
-
-#EBS Variable
-
-variable "availability_zone" {
-  type = string
-}
-
-variable "instance_id" {
-  type = string
-}
-
-variable "root_vol" {
-  type = number
-}
- 
-# EBS 
-# variable "root_block_device" {
-#   description = "List of EBS volume maps"
-#   type = list(object({
-#     device_name = string
-#     volume_size = number
-#     volume_type = string
-#     encrypted   = bool
-#   }))
+# variable "instance_name" {
+#   type = string
 # }
 
-#S3 Variable
+# variable "ami_id" {
+#   type = string
+# }
 
-variable "name" {
-    type = string
-}
+# variable "instance_type" {
+#   type = string
+# }
 
-variable "vpc_id" {
-    type = string
-}
+# #EBS Variable
 
-variable "lifecycle_days" {
-  type = number
-}
+# variable "availability_zone" {
+#   type = string
+# }
 
-#Security_Group Variables
+# variable "instance_id" {
+#   type = string
+# }
 
-variable "sg1_ingress_rules" {
-  description = "List of ingress rules"
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = list(string)
-  }))
-  }
+# variable "root_vol" {
+#   type = number
+# }
+ 
+# # EBS 
+# # variable "root_block_device" {
+# #   description = "List of EBS volume maps"
+# #   type = list(object({
+# #     device_name = string
+# #     volume_size = number
+# #     volume_type = string
+# #     encrypted   = bool
+# #   }))
+# # }
 
-variable "sg2_ingress_rules" {
-  description = "List of ingress rules"
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = list(string)
-  }))
-  }
+# #S3 Variable
+
+# variable "name" {
+#     type = string
+# }
+
+# variable "vpc_id" {
+#     type = string
+# }
+
+# variable "lifecycle_days" {
+#   type = number
+# }
+
+# #Security_Group Variables
+
+# variable "sg1_ingress_rules" {
+#   description = "List of ingress rules"
+#   type = list(object({
+#     from_port   = number
+#     to_port     = number
+#     protocol    = string
+#     cidr_blocks = list(string)
+#   }))
+#   }
+
+# variable "sg2_ingress_rules" {
+#   description = "List of ingress rules"
+#   type = list(object({
+#     from_port   = number
+#     to_port     = number
+#     protocol    = string
+#     cidr_blocks = list(string)
+#   }))
+#   }
