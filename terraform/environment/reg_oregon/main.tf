64c9eeb200a7f2d5a131504eb096bd4f483aa395
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-07-08 17:26:05 +0530

    post - Task 2 Full Code execution

--- terraform/environment/reg_oregon/main.tf
+++ terraform/environment/reg_oregon/main.tf
@@ -1,40 +1,40 @@
-module "ebs" {
-  source    = "../../module/ebs"
+# module "ebs" {
+#   source    = "../../module/ebs"
 
-  availability_zone  = module.ec2.EC2_availabilty_zone
-  instance_id        = module.ec2.Ec2_Instance_id
-}
+#   availability_zone  = module.ec2.EC2_availabilty_zone
+#   instance_id        = module.ec2.Ec2_Instance_id
+# }
 
-module "ec2" {
-  source    = "../../module/ec2"
-  root_vol = var.root_vol
-  instance_name     = var.instance_name
-  instance_type     = var.instance_type
-  ami_id            = var.ami_id
-  security_group_ids1 = [module.sg.SG_id1]
-  security_group_ids2 = [module.sg.SG_id2]
-}
+# module "ec2" {
+#   source    = "../../module/ec2"
+#   root_vol = var.root_vol
+#   instance_name     = var.instance_name
+#   instance_type     = var.instance_type
+#   ami_id            = var.ami_id
+#   security_group_ids1 = [module.sg.SG_id1]
+#   security_group_ids2 = [module.sg.SG_id2]
+# }
 
-module "s3" {
-  source = "../../module/s3"
+# module "s3" {
+#   source = "../../module/s3"
 
-  name    = var.name
-  vpc_id  = var.vpc_id
-  lifecycle_days = var.lifecycle_days
-}
+#   name    = var.name
+#   vpc_id  = var.vpc_id
+#   lifecycle_days = var.lifecycle_days
+# }
 
-module "sg" {
-  source             = "../../module/sg"
-  vpc_id             = module.s3.vpc_id
-  sg1_ingress_rules  = var.sg1_ingress_rules
-  sg2_ingress_rules  = var.sg2_ingress_rules
-}
+# module "sg" {
+#   source             = "../../module/sg"
+#   vpc_id             = module.s3.vpc_id
+#   sg1_ingress_rules  = var.sg1_ingress_rules
+#   sg2_ingress_rules  = var.sg2_ingress_rules
+# }
 
 
-output "EC2_availabilty_zone" {
-  value = module.ec2.EC2_availabilty_zone
-}
+# output "EC2_availabilty_zone" {
+#   value = module.ec2.EC2_availabilty_zone
+# }
 
-output "Ec2_Instance_id" {
-  value = module.ec2.Ec2_Instance_id
-}
+# output "Ec2_Instance_id" {
+#   value = module.ec2.Ec2_Instance_id
+# }
--- terraform/environment/reg_oregon/variable.tf
+++ terraform/environment/reg_oregon/variable.tf
@@ -1,74 +1,74 @@
-# #EC2 variable
+#EC2 variable
 
-# variable "instance_name" {
-#   type = string
-# }
+variable "instance_name" {
+  type = string
+}
 
-# variable "ami_id" {
-#   type = string
-# }
+variable "ami_id" {
+  type = string
+}
 
-# variable "instance_type" {
-#   type = string
-# }
+variable "instance_type" {
+  type = string
+}
 
-# #EBS Variable
+#EBS Variable
 
-# variable "availability_zone" {
-#   type = string
-# }
+variable "availability_zone" {
+  type = string
+}
 
-# variable "instance_id" {
-#   type = string
-# }
+variable "instance_id" {
+  type = string
+}
 
-# variable "root_vol" {
-#   type = number
-# }
+variable "root_vol" {
+  type = number
+}
  
-# # EBS 
-# # variable "root_block_device" {
-# #   description = "List of EBS volume maps"
-# #   type = list(object({
-# #     device_name = string
-# #     volume_size = number
-# #     volume_type = string
-# #     encrypted   = bool
-# #   }))
-# # }
-
-# #S3 Variable
-
-# variable "name" {
-#     type = string
-# }
-
-# variable "vpc_id" {
-#     type = string
-# }
-
-# variable "lifecycle_days" {
-#   type = number
-# }
-
-# #Security_Group Variables
-
-# variable "sg1_ingress_rules" {
-#   description = "List of ingress rules"
+# EBS 
+# variable "root_block_device" {
+#   description = "List of EBS volume maps"
 #   type = list(object({
-#     from_port   = number
-#     to_port     = number
-#     protocol    = string
-#     cidr_blocks = list(string)
+#     device_name = string
+#     volume_size = number
+#     volume_type = string
+#     encrypted   = bool
 #   }))
-#   }
+# }
 
-# variable "sg2_ingress_rules" {
-#   description = "List of ingress rules"
-#   type = list(object({
-#     from_port   = number
-#     to_port     = number
-#     protocol    = string
-#     cidr_blocks = list(string)
-#   }))
-#   }
+#S3 Variable
+
+variable "name" {
+    type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "lifecycle_days" {
+  type = number
+}
+
+#Security_Group Variables
+
+variable "sg1_ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  }
+
+variable "sg2_ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  }
