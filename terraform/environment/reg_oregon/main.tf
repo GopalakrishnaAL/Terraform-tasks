e03705b1d72b8fc6fc1a0e11aa46e66feff76e18
Gopala Krishna Arumugam <gopala.arumugam@tpnyc.local>
2025-08-01 06:36:39 +0530

    pushing file to github repo

--- terraform/environment/reg_oregon/main.tf
+++ terraform/environment/reg_oregon/main.tf
@@ -1,79 +1,79 @@
-# module "ebs" {
-#   source    = "../../module/ebs"
+module "ebs" {
+  source    = "../../module/ebs"
 
-#   availability_zone  = module.ec2.EC2_availabilty_zone
-#   instance_id        = module.ec2.Ec2_Instance_id
+  availability_zone  = module.ec2.EC2_availabilty_zone
+  instance_id        = module.ec2.Ec2_Instance_id
+}
+
+module "ec2" {
+  source    = "../../module/ec2"
+  root_vol = var.root_vol
+  instance_name     = var.instance_name
+  instance_type     = var.instance_type
+  ami_id            = var.ami_id
+  security_group_ids1 = [module.sg.SG_id1]
+  security_group_ids2 = [module.sg.SG_id2]
+}
+
+# module "s3" {
+#   source = "../../module/s3"
+
+#   name    = var.name
+#   vpc_id  = var.vpc_id
+#   lifecycle_days = var.lifecycle_days
 # }
 
-# module "ec2" {
-#   source    = "../../module/ec2"
-#   root_vol = var.root_vol
-#   instance_name     = var.instance_name
-#   instance_type     = var.instance_type
-#   ami_id            = var.ami_id
-#   security_group_ids1 = [module.sg.SG_id1]
-#   security_group_ids2 = [module.sg.SG_id2]
-# }
+module "sg" {
+  source             = "../../module/sg"
+  vpc_id             = var.vpc_id
+  sg1_ingress_rules  = var.sg1_ingress_rules
+  sg2_ingress_rules  = var.sg2_ingress_rules
+}
 
-# # module "s3" {
-# #   source = "../../module/s3"
+module "vpc" {
+  source = "../../module/vpc"
+  vpc_id = var.vpc_id
+}
 
-# #   name    = var.name
-# #   vpc_id  = var.vpc_id
-# #   lifecycle_days = var.lifecycle_days
-# # }
+output "EC2_availabilty_zone" {
+  value = module.ec2.EC2_availabilty_zone
+}
 
-# module "sg" {
-#   source             = "../../module/sg"
-#   vpc_id             = var.vpc_id
-#   sg1_ingress_rules  = var.sg1_ingress_rules
-#   sg2_ingress_rules  = var.sg2_ingress_rules
-# }
+output "Ec2_Instance_id" {
+  value = module.ec2.Ec2_Instance_id
+}
 
-# module "vpc" {
-#   source = "../../module/vpc"
-#   vpc_id = var.vpc_id
-# }
+module "cloudwatch" {
+  source = "../../module/cloudwatch"
 
-# output "EC2_availabilty_zone" {
-#   value = module.ec2.EC2_availabilty_zone
-# }
+  alarm_name          = var.alarm_name
+  alarm_description   = var.alarm_description
+  comparison_operator = var.comparison_operator
+  evaluation_periods  = var.evaluation_periods
+  datapoints_to_alarm = var.datapoints_to_alarm
+  metric_name         = var.metric_name
+  period              = var.period
+  statistic           = var.statistic
+  threshold           = var.threshold
+  InstanceId          = module.ec2.Ec2_Instance_id
+}
 
-# output "Ec2_Instance_id" {
-#   value = module.ec2.Ec2_Instance_id
-# }
+module "aws_cloudwatch_dashboard" {
+  source = "../../module/cloudwatch dashboard"
 
-# module "cloudwatch" {
-#   source = "../../module/cloudwatch"
+  dashboard_name        = var.dashboard_name
+  dashboard_aws_region  = var.dashboard_aws_region
+  dashboard_instance_id = module.ec2.Ec2_Instance_id
+}
 
-#   alarm_name          = var.alarm_name
-#   alarm_description   = var.alarm_description
-#   comparison_operator = var.comparison_operator
-#   evaluation_periods  = var.evaluation_periods
-#   datapoints_to_alarm = var.datapoints_to_alarm
-#   metric_name         = var.metric_name
-#   period              = var.period
-#   statistic           = var.statistic
-#   threshold           = var.threshold
-#   InstanceId          = module.ec2.Ec2_Instance_id
-# }
+module "waf" {
+  source = "../../module/waf"
 
-# module "aws_cloudwatch_dashboard" {
-#   source = "../../module/cloudwatch dashboard"
-
-#   dashboard_name        = var.dashboard_name
-#   dashboard_aws_region  = var.dashboard_aws_region
-#   dashboard_instance_id = module.ec2.Ec2_Instance_id
-# }
-
-# module "waf" {
-#   source = "../../module/waf"
-
-#   name             = var.waf_name
-#   description      = var.waf_description
-#   scope            = var.waf_scope
-#   managed_rules    = var.waf_managed_rules
-#   custom_rules     = var.waf_custom_rules
-#   custom_or_rules  = var.waf_custom_or_rules
-#   tags             = var.tags
-# }
+  name             = var.waf_name
+  description      = var.waf_description
+  scope            = var.waf_scope
+  managed_rules    = var.waf_managed_rules
+  custom_rules     = var.waf_custom_rules
+  custom_or_rules  = var.waf_custom_or_rules
+  tags             = var.tags
+}
--- terraform/oregon_main.tf
+++ terraform/oregon_main.tf
@@ -1,143 +1,144 @@
-# data "aws_vpc" "default" {
-#   provider = aws.oregon
-#   default  = true
-# }
+data "aws_vpc" "default" {
+  provider = aws.oregon
+  default  = true
+}
 
-# module "reg_oregon" {
-#   source = "./environment/reg_oregon"
+module "reg_oregon" {
+  source = "./environment/reg_oregon"
 
-#   providers = { aws = aws.oregon }
+  providers = { aws = aws.oregon }
 
-#   # VPC
-#   vpc_id         = data.aws_vpc.default.id
+  # VPC
+  vpc_id         = data.aws_vpc.default.id
 
-#   # EC2 Instance Configuration
-#   instance_name     = "test_instance"
-#   ami_id            = "ami-0992e77a19022a98d"
-#   instance_type     = "t3.micro"
-#   availability_zone = module.reg_oregon.EC2_availabilty_zone
-#   instance_id       = module.reg_oregon.Ec2_Instance_id
+  # EC2 Instance Configuration
+  instance_name     = "test_instance"
+  ami_id            = "ami-0992e77a19022a98d"
+  instance_type     = "t3.micro"
+  availability_zone = module.reg_oregon.EC2_availabilty_zone
+  instance_id       = module.reg_oregon.Ec2_Instance_id
 
-#   root_vol = "80"
 
-#   # Security Group Rules
-#   sg1_ingress_rules = [ 
+  root_vol = "80"
+
+  # Security Group Rules
+  sg1_ingress_rules = [ 
+    {
+      from_port   = 95,
+      to_port     = 95,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "RDP access"
+    }
+  ]
+  sg2_ingress_rules = [
+    {
+      from_port   = 12,
+      to_port     = 12,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      description = "RDP access"
+    }    
+  ]
+
+ # S3 Bucket Configuration
+  name           = "gopal-1-bucket"
+  lifecycle_days = 15
+
+#   EBS Volumes
+#   reg_ebs_volumes = [
 #     {
-#       from_port   = 95,
-#       to_port     = 95,
-#       protocol    = "tcp",
-#       cidr_blocks = ["0.0.0.0/0"],
-#       description = "RDP access"
-#     }
-#   ]
-#   sg2_ingress_rules = [
-#     {
-#       from_port   = 12,
-#       to_port     = 12,
-#       protocol    = "tcp",
-#       cidr_blocks = ["0.0.0.0/0"],
-#       description = "RDP access"
-#     }    
-#   ]
-
-#  # S3 Bucket Configuration
-#   name           = "gopal-1-bucket"
-#   lifecycle_days = 15
-
-# #   EBS Volumes
-# #   reg_ebs_volumes = [
-# #     {
-# #       device_name = "/dev/sdh",
-# #       volume_size = 30,
-# #       volume_type = "gp3"
-# #       encrypted = true
-# #     },
-# #     {
-# #       device_name = "/dev/sdi",
-# #       volume_size = 20,
-# #       volume_type = "gp3"
-# #       encrypted = true
-# #     }
-# #   ]
-
-# #CloudWatch CPU_Utilization
-#   alarm_name          = "CPU_Utilization"
-#   alarm_description   = "CPU_Utilzation_Crossed_90%"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 5
-#   datapoints_to_alarm = 3
-#   metric_name         = "CPUUtilization"
-#   period              = 60
-#   statistic           = "Maximum"
-#   threshold           = 90
-
-# #CloudWatch Dashboard
-#   dashboard_name        = "Oregon_EC2_Dashboard"
-#   dashboard_aws_region  = "us-west-2"
-
-# #WAF
-#   waf_name        = "waf-oregon"
-#   waf_description = "Initial WAF deployment for oregon region"
-#   waf_scope       = "REGIONAL"
-
-#   waf_managed_rules = [
-#     {
-#       name            = "AWSManagedRulesCommonRuleSet"
-#       priority        = 1
-#       rule_group_name = "AWSManagedRulesCommonRuleSet"
-#       vendor_name     = "AWS"
+#       device_name = "/dev/sdh",
+#       volume_size = 30,
+#       volume_type = "gp3"
+#       encrypted = true
 #     },
 #     {
-#       name            = "AWSManagedRulesAmazonIpReputationList"
-#       priority        = 2
-#       rule_group_name = "AWSManagedRulesAmazonIpReputationList"
-#       vendor_name     = "AWS"
+#       device_name = "/dev/sdi",
+#       volume_size = 20,
+#       volume_type = "gp3"
+#       encrypted = true
 #     }
 #   ]
 
-#   waf_custom_rules = [
-#     {
-#     name                  = "block-host-header"
-#     priority              = 3
-#     action                = "block"
-#     match_field           = "host"           
-#     positional_constraint = "EXACTLY"
-#     search_string         = "Gopal.com"
-#     }
-#   ]
+#CloudWatch CPU_Utilization
+  alarm_name          = "CPU_Utilization"
+  alarm_description   = "CPU_Utilzation_Crossed_90%"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 5
+  datapoints_to_alarm = 3
+  metric_name         = "CPUUtilization"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 90
 
-#   waf_custom_or_rules = [
-#     {
-#       name     = "AllowSpecificHosts"
-#       priority = 4
-#       action   = "allow"
-#       statements = [
-#         {
-#           match_field           = "host"
-#           positional_constraint = "STARTS_WITH"
-#           search_string         = "gopala.in"
-#         },
-#         {
-#           match_field           = "host"
-#           positional_constraint = "CONTAINS"
-#           search_string         = "gopal.com"
-#         },
-#         {
-#           match_field           = "host"
-#           positional_constraint = "ENDS_WITH"
-#           search_string         = "gopal.org"
-#         },
-#         {
-#           match_field           = "host"
-#           positional_constraint = "EXACTLY"
-#           search_string         = "gopal.uk"
-#         }
-#       ]
-#     }
-#   ]
+#CloudWatch Dashboard
+  dashboard_name        = "Oregon_EC2_Dashboard"
+  dashboard_aws_region  = "us-west-2"
 
-#   tags = {
-#     Environment = "P"
-#     Project     = "Oregon"
-#   }
-# }
+#WAF
+  waf_name        = "waf-oregon"
+  waf_description = "Initial WAF deployment for oregon region"
+  waf_scope       = "REGIONAL"
+
+  waf_managed_rules = [
+    {
+      name            = "AWSManagedRulesCommonRuleSet"
+      priority        = 1
+      rule_group_name = "AWSManagedRulesCommonRuleSet"
+      vendor_name     = "AWS"
+    },
+    {
+      name            = "AWSManagedRulesAmazonIpReputationList"
+      priority        = 2
+      rule_group_name = "AWSManagedRulesAmazonIpReputationList"
+      vendor_name     = "AWS"
+    }
+  ]
+
+  waf_custom_rules = [
+    {
+    name                  = "block-host-header"
+    priority              = 3
+    action                = "block"
+    match_field           = "host"           
+    positional_constraint = "EXACTLY"
+    search_string         = "Gopal.com"
+    }
+  ]
+
+  waf_custom_or_rules = [
+    {
+      name     = "AllowSpecificHosts"
+      priority = 4
+      action   = "allow"
+      statements = [
+        {
+          match_field           = "host"
+          positional_constraint = "STARTS_WITH"
+          search_string         = "gopala.in"
+        },
+        {
+          match_field           = "host"
+          positional_constraint = "CONTAINS"
+          search_string         = "gopal.com"
+        },
+        {
+          match_field           = "host"
+          positional_constraint = "ENDS_WITH"
+          search_string         = "gopal.org"
+        },
+        {
+          match_field           = "host"
+          positional_constraint = "EXACTLY"
+          search_string         = "gopal.uk"
+        }
+      ]
+    }
+  ]
+
+  tags = {
+    Environment = "P"
+    Project     = "Oregon"
+  }
+}
